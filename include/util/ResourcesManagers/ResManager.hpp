c346b4cb3b1c00956b187d9cb16dad2e90928016
Benoît CORTIER <benoit.cortier@gmail.com>
2014-12-12 23:49:27 +0100

    Partialy rewrited ResourcesManager class.

---
+++ include/util/ResourcesManagers/ResManager.hpp
@@ -0,0 +1,22 @@
+#ifndef AUTOMATEDRESMANAGER_HPP_INCLUDED
+#define AUTOMATEDRESMANAGER_HPP_INCLUDED
+
+#include <memory>
+#include <map>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+#include "util/ResourcesManagers/BaseResManager.hpp"
+
+namespace mp
+{
+    /** A class to automatically handle resources through the program. */
+
+    class ResManager : public BaseResManager
+    {
+
+    };
+}
+
+#endif // AUTOMATEDRESMANAGER_HPP_INCLUDED
