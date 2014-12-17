2cd58cb6d1571dc9f76b4fc82629970f4951f25d
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2014-12-17 15:49:35 +0100

    Added write_header functions

--- include/encryption/crypt_function.hpp
+++ include/encryption/crypt_function.hpp
@@ -14,6 +14,8 @@ namespace mp
 {
     std::string crypt_key(std::string key);
     std::string crypt_data(std::string key, std::string data);
+
+    void write_header(std::string output_file, std::vector<std::string> filename, std::string key);
 }
 
 #endif // CRYPT_FUNCTION_INCLUDED
--- src/encryption/crypt_functions.cpp
+++ src/encryption/crypt_functions.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 
 #include "encryption/crypt_function.hpp"
+#include "util/file_functions.hpp"
 
 using namespace std;
 
@@ -20,4 +21,66 @@ string mp::crypt_data(string key, string data)
     for(unsigned int i=0; i<data.length(); i++)
         data[i] = static_cast<char>( data[i] ^ key[i%key.length()] );
     return data;
+}
+
+void mp::write_header(string output_file, vector<string> filenames, string key)
+{
+    //Set base offset to 8 bytes (headerlen + filecount).
+    long int offset=8, filecount, fileoffset;
+    int headerlen;
+
+    vector<long int> filesize, filenamelen, boffset;
+
+    fstream output(output_file, ios::in | ios::out | ios::binary | ios::trunc);
+
+    //Encrypt the key.
+    key = crypt_key(key);
+
+    for(unsigned int i = 0; i<filenames.size(); i++)
+    {
+        //Calculate the filename length and push it in filename.
+        filenamelen.push_back(filenames[i].size());
+        filesize.push_back(get_file_size(filenames[i]));
+
+        // Write the length of the filename on 4 bytes.
+        output.seekg(offset,ios::beg);
+        output.write(reinterpret_cast<char*>(&filenamelen[i]),4);
+
+        // Write the length of the filename on 4 bytes.
+        output << filenames[i];
+        output.write(reinterpret_cast<char*>(&filesize[i]),4);
+
+        // Increment offset (filename length + 4 bytes for write filenamelen
+        // 4 bytes for the size and 4 bytes for the offset).
+        offset+=filenamelen[i]+4+4+4;
+    }
+
+    output.seekg(0,ios::beg);
+
+    //Add the last offset in headerlen and write it.
+    headerlen = get_file_size(output_file)+4;
+    output.write(reinterpret_cast<char*>(&headerlen),4);
+
+    //Write the number of files which are encrypted.
+    filecount = filenames.size();
+    output.write(reinterpret_cast<char*>(&filecount),4);
+
+    //Set base offset to 4, and base fileoffset to headerlen.
+    offset=4;
+	fileoffset = headerlen;
+
+    for(unsigned int i = 0; i<filenames.size(); i++)
+    {
+        offset+=filenames[i].size()+4+4+4;
+        boffset.push_back(fileoffset);
+
+        //Write fileoffset
+        output.seekg(offset,ios::beg);
+        output.write(reinterpret_cast<const char*>(&fileoffset),4);
+
+        fileoffset += filesize[i];
+    }
+    output.close();
+
+
 }
