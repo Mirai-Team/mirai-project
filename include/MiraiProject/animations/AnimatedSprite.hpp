5a4d218611b4aa65cafe794aa7cc5f8cadc92052
Benoît CORTIER <benoit.cortier@gmail.com>
2015-01-11 17:03:08 +0100

    Added AnimatedSprite class.

---
+++ include/MiraiProject/animations/AnimatedSprite.hpp
@@ -0,0 +1,108 @@
+////////////////////////////////////////////////////////////
+//
+// MiraiProject
+// Copyright (C) 2014-2015 CORTIER Benoît (benoit.cortier@gmail.com), BOULMIER Jérôme (jerome.boulmier@outlook.com)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+// you must not claim that you wrote the original software.
+// If you use this software in a product, an acknowledgment
+// in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+// and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef ANIMATEDSPRITE_HPP_INCLUDED
+#define ANIMATEDSPRITE_HPP_INCLUDED
+
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/System/Time.hpp>
+
+/** @file AnimatedSprite.hpp
+ * \brief This file define AnimatedSprite class.
+ */
+
+namespace mp
+{
+	class AnimatedSprite : public sf::Drawable, public sf::Transformable
+	{
+		public:
+			/** \brief Constructor */
+			AnimatedSprite();
+			
+			AnimatedSprite(const sf::Texture& texture);
+
+			/** \brief Destructor */
+			virtual ~AnimatedSprite();
+			
+			void update(sf::Time dt);
+			
+			/////////////
+			// Getters //
+			/////////////
+			
+			sf::Vector2i getFrameSize() const;
+			
+			int getNumFrames() const;
+			
+			int getCurrentFrame() const;
+			
+			bool isRepeating() const;
+			
+			sf::Time getTimePerFrame() const;
+			
+			bool isFinished() const;
+
+			const sf::Texture* getTexture() const;
+
+			sf::FloatRect getLocalBounds() const;
+
+			sf::FloatRect getGlobalBounds() const;
+			
+			/////////////
+			// Setters //
+			/////////////
+			
+			void setFrameSize(sf::Vector2i newFrameSize);
+			
+			void setNumFrames(int newNumFrames);
+			
+			void setCurrentFrame(int newCurrentFrame);
+			
+			void restart();
+			
+			void setRepeating(bool newRepeat);
+			
+			void setTimePerFrame(sf::Time newTimePerFrame);
+
+			void setTexture(const sf::Texture& texture);
+			
+		private:
+			sf::Sprite sprite;
+			
+			sf::Vector2i frameSize;
+			
+			int numFrames;
+			int currentFrame;
+			
+			bool repeat;
+			
+			sf::Time timePerFrame;
+			sf::Time elapsedTime;
+			
+			virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+	};
+}
+
+#endif // ANIMATEDSPRITE_HPP_INCLUDED
---
+++ src/animations/AnimatedSprite.cpp
@@ -0,0 +1,194 @@
+////////////////////////////////////////////////////////////
+//
+// MiraiProject
+// Copyright (C) 2014-2015 CORTIER Benoît (benoit.cortier@gmail.com), BOULMIER Jérôme (jerome.boulmier@outlook.com)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+// you must not claim that you wrote the original software.
+// If you use this software in a product, an acknowledgment
+// in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+// and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/System/Time.hpp>
+
+#include "MiraiProject/animations/AnimatedSprite.hpp"
+
+mp::AnimatedSprite::AnimatedSprite() :
+	sprite{ },
+	
+	frameSize{ 64, 64 },
+	
+	numFrames{ 0 },
+	currentFrame{ 0 },
+	
+	repeat{ false },
+	
+	timePerFrame{ sf::Time::Zero },
+	elapsedTime{ sf::Time::Zero }
+{
+	
+}
+
+mp::AnimatedSprite::AnimatedSprite(const sf::Texture& texture) : 
+	sprite{ texture },
+	
+	frameSize{ 64, 64 },
+	
+	numFrames{ 0 },
+	currentFrame{ 0 },
+	
+	repeat{ false },
+	
+	timePerFrame{ sf::Time::Zero },
+	elapsedTime{ sf::Time::Zero }
+{
+	
+}
+
+mp::AnimatedSprite::~AnimatedSprite()
+{
+	
+}
+
+void mp::AnimatedSprite::update(sf::Time dt)
+{
+	sf::Vector2i textureBounds(sprite.getTexture()->getSize());
+	sf::IntRect textureRect{ sprite.getTextureRect() };
+	
+	elapsedTime += dt;
+	
+	while (elapsedTime >= timePerFrame and (currentFrame + 1 < numFrames or repeat))
+	{
+		elapsedTime -= timePerFrame;
+		
+		if (repeat)
+			currentFrame = (currentFrame + 1) % numFrames;
+		else
+			currentFrame++;
+	}
+	
+	textureRect.left = (frameSize.x * currentFrame) % textureBounds.x;
+	textureRect.top = frameSize.y * static_cast<int>((frameSize.x * currentFrame) / textureBounds.x);
+	
+	if (currentFrame == 0)
+		textureRect = sf::IntRect(0, 0, frameSize.x, frameSize.y);
+	
+	sprite.setTextureRect(textureRect);
+}
+
+/////////////
+// Getters //
+/////////////
+
+sf::Vector2i mp::AnimatedSprite::getFrameSize() const
+{
+	return frameSize;
+}
+
+int mp::AnimatedSprite::getNumFrames() const
+{
+	return numFrames;
+}
+
+int mp::AnimatedSprite::getCurrentFrame() const
+{
+	return currentFrame;
+}
+
+bool mp::AnimatedSprite::isRepeating() const
+{
+	return repeat;
+}
+
+sf::Time mp::AnimatedSprite::getTimePerFrame() const
+{
+	return timePerFrame;
+}
+
+bool mp::AnimatedSprite::isFinished() const
+{
+	return currentFrame >= numFrames;
+}
+
+const sf::Texture* mp::AnimatedSprite::getTexture() const
+{
+	return sprite.getTexture();
+}
+
+sf::FloatRect mp::AnimatedSprite::getLocalBounds() const
+{
+	return sf::FloatRect(getOrigin(), static_cast<sf::Vector2f>(getFrameSize()));
+}
+
+sf::FloatRect mp::AnimatedSprite::getGlobalBounds() const
+{
+	return getTransform().transformRect(getLocalBounds());
+}
+
+/////////////
+// Setters //
+/////////////
+
+void mp::AnimatedSprite::setFrameSize(sf::Vector2i newFrameSize)
+{
+	frameSize = newFrameSize;
+}
+
+void mp::AnimatedSprite::setNumFrames(int newNumFrames)
+{
+	numFrames = newNumFrames;
+}
+
+void mp::AnimatedSprite::setCurrentFrame(int newCurrentFrame)
+{
+	currentFrame = newCurrentFrame;
+}
+
+void mp::AnimatedSprite::restart()
+{
+	currentFrame = 0;
+}
+
+void mp::AnimatedSprite::setRepeating(bool newRepeat)
+{
+	repeat = newRepeat;
+}
+
+void mp::AnimatedSprite::setTimePerFrame(sf::Time newTimePerFrame)
+{
+	timePerFrame = newTimePerFrame;
+}
+
+void mp::AnimatedSprite::setTexture(const sf::Texture& texture)
+{
+	sprite.setTexture(texture);
+}
+
+/////////////
+// Private //
+/////////////
+
+void mp::AnimatedSprite::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	states.transform *= getTransform();
+	target.draw(sprite, states);
+}
