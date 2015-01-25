b9c71f5fd76bb905ab327250ff51493833c42ce5
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2015-01-25 02:31:04 +0100

    Updated CMakeLists. Added macros for CMake.

---
+++ cmake/genrev.cmake
@@ -0,0 +1,36 @@
+if(NOT BUILDDIR)
+  set(NO_GIT ${WITHOUT_GIT})
+  set(GIT_EXEC ${GIT_EXECUTABLE})
+  set(BUILDDIR ${CMAKE_BINARY_DIR})
+endif()
+
+if(NO_GIT)
+  set(rev_date "1970-01-01 00:00:00 +0000")
+  set(rev_hash "unknown")
+  set(rev_branch "unknown")
+else()
+  if(GIT_EXEC)
+    execute_process(
+      COMMAND "${GIT_EXEC}" rev-parse HEAD
+      WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+      OUTPUT_VARIABLE revHash
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      ERROR_QUIET
+    )
+    execute_process(
+      COMMAND "${GIT_EXEC}" show -s --format=%ci
+      WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+      OUTPUT_VARIABLE revDate
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      ERROR_QUIET
+    )
+
+    execute_process(
+      COMMAND "${GIT_EXEC}" rev-parse --abbrev-ref HEAD
+      WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+      OUTPUT_VARIABLE revBranch
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      ERROR_QUIET
+    )
+  endif()
+endif()
---
+++ cmake/options.cmake
@@ -0,0 +1,4 @@
+option(MIRAI_WITH_WARNINGS      "Show all warnings during compile"  1)
+option(MIRAI_WITH_GIT           "Enable git routine"                1)
+option(MIRAI_TOOLS              "Build tools"                       0)
+option(MIRAI_EXAMPLES           "Build examples"                    0)
