960c9db0a23fa09635cd19f04fa1ff5c8052d6ad
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2015-02-21 22:28:28 +0100

    Added Clickable Widget.

---
+++ include/MiraiProject/gui/ClickableWidget.hpp
@@ -0,0 +1,56 @@
+////////////////////////////////////////////////////////////
+//
+// MiraiProject
+// Copyright (C) 2014-2015 CORTIER Benoît (benoit.cortier@gmail.com), BOULMIER Jérôme (jerome.boulmier@outlook.com)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+// you must not claim that you wrote the original software.
+// If you use this software in a product, an acknowledgment
+// in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+// and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef CLICKABE_WIDGET_HPP_INCLUDED
+#define CLICKABE_WIDGET_HPP_INCLUDED
+
+#include <SFML/Window/Mouse.hpp>
+
+#include "MiraiProject/gui/Widget.hpp"
+
+namespace mp 
+{
+    class ClickableWidget : public Widget
+    {
+        public:
+            ClickableWidget();
+
+            void setSize(sf::Vector2f size);
+
+            sf::Vector2f getSize();
+
+            virtual bool mouseOnWidget();
+
+            virtual bool isPressed();
+
+            virtual bool isReleased();
+        private:
+            bool isPressed_;
+
+            sf::Vector2f size_;
+
+    };
+}
+
+#endif
---
+++ src/gui/ClickableWidget.cpp
@@ -0,0 +1,70 @@
+////////////////////////////////////////////////////////////
+//
+// MiraiProject
+// Copyright (C) 2014-2015 CORTIER Benoît (benoit.cortier@gmail.com), BOULMIER Jérôme (jerome.boulmier@outlook.com)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+// you must not claim that you wrote the original software.
+// If you use this software in a product, an acknowledgment
+// in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+// and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
+#include "MiraiProject/gui/ClickableWidget.hpp"
+
+mp::ClickableWidget::ClickableWidget() :    isPressed_ { },
+                                            size_ { }
+{
+
+}
+
+void mp::ClickableWidget::setSize(sf::Vector2f size)
+{
+    size_= size;
+}
+
+sf::Vector2f mp::ClickableWidget::getSize()
+{
+    return size_;
+}
+
+bool mp::ClickableWidget::mouseOnWidget()
+{
+    sf::Vector2f mousePosition(sf::Mouse::getPosition());
+    sf::FloatRect rect(0, 0, getSize().x, getSize().y);
+    if(getTransform().transformRect(rect).contains(mousePosition))
+        return true;
+    else
+        return false;
+}
+
+bool mp::ClickableWidget::isPressed()
+{
+    if(mouseOnWidget() && sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+    {
+        isPressed_ = true;
+        return true;
+    }
+    else
+        return false;
+}
+
+bool mp::ClickableWidget::isReleased()
+{
+    if(isPressed_ && mouseOnWidget())
+        return true;
+    else
+        return false;
+}
