274a3fabed97d0311573459383dc05c5fac1a498
Benoît CORTIER <benoit.cortier@gmail.com>
2015-05-20 22:57:25 +0200

    Added TextBox class (version 1).

--- examples/gui/src/main.cpp
+++ examples/gui/src/main.cpp
@@ -5,11 +5,12 @@
 
 #include <MiraiProject/gui/Border.hpp>
 #include <MiraiProject/gui/Button.hpp>
-#include <MiraiProject/gui/Image.hpp>
 #include <MiraiProject/gui/Label.hpp>
+#include <MiraiProject/gui/TextBox.hpp>
 #include <MiraiProject/resourcesManagers/AutoResManager.hpp>
 #include <MiraiProject/util/WindowManager.hpp>
-#include <SFML/Graphics.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
 
 void helloWorld(mp::Label* label, mp::Border* border)
 {
@@ -26,12 +27,12 @@ int main()
     sf::RenderWindow &window = mainWindowManager.getWindow();
 
     mainWindowManager.setWindowName("Simple Window Test");
-    mainWindowManager.setOptimalWinWidth(300);
-    mainWindowManager.setOptimalWinHeight(300);
-    mainWindowManager.setVideomodeWidth(300);
-    mainWindowManager.setVideomodeHeight(300);
-
-    mp::Button button;
+    mainWindowManager.setOptimalWinWidth(800);
+    mainWindowManager.setOptimalWinHeight(600);
+    mainWindowManager.setVideomodeWidth(800);
+    mainWindowManager.setVideomodeHeight(600);
+    mainWindowManager.setCursorVisible(true);
+    mainWindowManager.create();
 
     mp::Label* label = new mp::Label();
     label->setText(L"Mirai Project Logo");
@@ -42,13 +43,13 @@ int main()
     mp::Label::childPtr labelNode(label);
 
     mp::Border* border = new mp::Border();
-
     border->createRectangle(220.f, 220.f, sf::Color::Red);
     border->createBlank(mp::Border::Edge::Bottom, 100, 10);
     border->createBlank(mp::Border::Edge::Left, 100, 10);
     border->setPosition(-10.f, -10.f);
     mp::Border::childPtr borderNode(border);
 
+    mp::Button button;
     button.setNormalTexture(resourcesManager.getTexture("resources/logo.png"));
     button.setDownTexture(resourcesManager.getTexture("resources/logoD.png"));
     button.setHoverTexture(resourcesManager.getTexture("resources/logoH.png"));
@@ -57,10 +58,14 @@ int main()
     button.addFrontChild(labelNode);
     button.addFrontChild(borderNode);
 
-    // Set borders' colour.
-    mainWindowManager.setBordersColor(sf::Color::White);
-    mainWindowManager.setCursorVisible(true);
-    mainWindowManager.create();
+    // Creating a text box.
+    mp::TextBox textBox;
+    textBox.setNormalTexture(resourcesManager.getTexture("resources/logo.png"));
+    textBox.setFocusTexture(resourcesManager.getTexture("resources/logoD.png"));
+    textBox.setHoverTexture(resourcesManager.getTexture("resources/logoH.png"));
+    textBox.setPosition(45.f, 310.f);
+    textBox.setFont(*resourcesManager.getFontBuffer("resources/UbuntuMono-R.ttf"));
+    textBox.setText(L"Some text.");
 
     // Useful only for testing purpose, but not very effective for real games.
     window.setFramerateLimit(60);
@@ -76,15 +81,20 @@ int main()
                 case sf::Event::Closed:
                     running = false;
                     break;
+                case sf::Event::TextEntered:
+                    textBox.handleInput(event.text.unicode);
+                    break;
                 default:
                     break;
             }
         }
 
         button.update(sf::Mouse::getPosition(window));
+        textBox.update(sf::Mouse::getPosition(window));
 
         window.clear(sf::Color::Blue);
         window.draw(button);
+        window.draw(textBox);
         window.display();
     }
     
