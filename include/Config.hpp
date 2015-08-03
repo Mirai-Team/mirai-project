644f02924c6c63b63da7d3fc210a661b0943299a
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2015-08-03 16:31:25 +0200

    Added Config.hpp

---
+++ include/Config.hpp
@@ -0,0 +1,18 @@
+#ifndef MP_CONFIG_HPP
+#define MP_CONFIG_HPP
+
+#include <cstdint>
+
+namespace mp {
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+} // mp
+
+#endif // MP_CONFIG_HPP
